7ef63d5e65ce0b5ac0d97b26827478aa85b5eccc
Pramsu Pandey <pramsu.pandey@outlook.com>
2026-09-15 18:02:44 -0400

    smth: smth

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@pbsoj/ui";
+
+function App() {
+  return (
+    <>
+    <Button appName="web"> </Button>
+    </>
+  )
+}
+
+export default App
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export default function Profile() {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
-};
+    
+  )
+}
